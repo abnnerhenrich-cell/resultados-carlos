aa0147f7cc164665d74e8b48f0fcfad733f4b797
abnnerhenrich-cell <abnner.henrich@gmail.com>
2026-06-29 10:17:10 -0300

    Update fontes.js

--- functions/fontes.js
+++ functions/fontes.js
@@ -3,12 +3,13 @@ module.exports = {
     "https://deunoposte.app.br/goias/",
     "https://www.eojogodobicho.com/resultado-jogo-bicho-look-goias.html"
   ],
+
   rio: [
     "https://deunoposte.app.br/rio-de-janeiro/",
     "https://www.resultadofacil.com.br/resultado-do-jogo-do-bicho/rj"
   ],
+
   nacional: [
-    "https://deunoposte.app.br/loteria-nacional/",
-    "https://www.resultadofacil.com.br/resultados-loteria-nacional-de-hoje"
+    "https://deunoposte.app.br/loteria-nacional/"
   ]
 };
