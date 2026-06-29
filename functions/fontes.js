a4d27b1cb8ca55159f090de8e771a399b6f50ea4
abnnerhenrich-cell <abnner.henrich@gmail.com>
2026-06-29 09:27:39 -0300

    Update fontes.js

--- functions/fontes.js
+++ functions/fontes.js
@@ -1,6 +1,14 @@
-module.exports={
-  // Fontes públicas; se alguma mudar o HTML, troque a URL aqui.
-  look:['https://deunoposte.app.br/goias/','https://www.eojogodobicho.com/resultado-jogo-bicho-look-goias.html'],
-  rio:['https://deunoposte.app.br/rio-de-janeiro/','https://www.resultadofacil.com.br/resultado-do-jogo-do-bicho/rj'],
-  nacional:['https://www.resultadofacil.com.br/resultados-loteria-nacional-de-hoje','https://deunoposte.app.br/loteria-nacional/']
+module.exports = {
+  look: [
+    "https://deunoposte.app.br/goias/",
+    "https://www.eojogodobicho.com/resultado-jogo-bicho-look-goias.html"
+  ],
+  rio: [
+    "https://deunoposte.app.br/rio-de-janeiro/",
+    "https://www.resultadofacil.com.br/resultado-do-jogo-do-bicho/rj"
+  ],
+  nacional: [
+    "https://deunoposte.app.br/loteria-nacional/",
+    "https://www.resultadofacil.com.br/resultados-loteria-nacional-de-hoje"
+  ]
 };
